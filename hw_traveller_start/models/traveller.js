f9d51e8e974bf188d0ad3855629612cd5f866513
Leszek (Les) <leszek.modrzejewski@gmail.com>
2022-02-16 17:01:47 +0000

    MVP and extensions

--- hw_traveller_start/models/traveller.js
+++ hw_traveller_start/models/traveller.js
@@ -3,27 +3,39 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  const startLocations = [];
+  this.journeys.forEach(journey => startLocations.push(journey.startLocation));
+  return startLocations;
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  const endLocations = [];
+  this.journeys.forEach(journey => endLocations.push(journey.endLocation));
+  return endLocations;
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
+  const journeysByTransport = this.journeys.filter(journey => journey.transport === transport);
+  return journeysByTransport;
 
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  const journeysByMinDistance = this.journeys.filter(journey => journey.distance > minDistance);
+  return journeysByMinDistance;
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  const totalDistanceTravelled = this.journeys.reduce((runningTotal, journey) => {
+    return runningTotal + journey.distance
+  }, 0);
+  return totalDistanceTravelled
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
+  return this.journeys.reduce((modesOfTransportList, journey) => {
+    modesOfTransportList.push(journey.transport)
+  }, [])
 };
 
 
